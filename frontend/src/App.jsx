3e97b9ea63c13c18787df1f66fc4f0b2cd480733
vansh132 <vanshji132@gmail.com>
2024-09-23 12:27:33 +0530

    chaned unused import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 import { Context } from "./main";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
